57a5834a8c984ac4914ca419b24e53a78cc9f50f
pkminor <minorpk@gmail.com>
2019-11-22 16:06:46 +0300

    added models and dao

---
+++ src/test/java/dao/Sql2oAnimalDaoTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oAnimalDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void clearAllAnimals() {
+    }
+}
---
+++ src/test/java/dao/Sql2oEndangeredAnimalDaoTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oEndangeredAnimalDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAllEndangeredAnimals() {
+    }
+
+    @Test
+    public void addEndangeredAnimal() {
+    }
+
+    @Test
+    public void findEndangeredAnimalById() {
+    }
+
+    @Test
+    public void updateEndangeredAnimal() {
+    }
+
+    @Test
+    public void clearAllEndangeredAnimals() {
+    }
+}
