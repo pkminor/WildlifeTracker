56eab422fbce81a7cc3c519b0bfc472a2924e35d
pkminor <minorpk@gmail.com>
2019-11-22 16:12:57 +0300

    added endagered animal dao

--- src/main/java/dao/EndangeredAnimalDao.java
+++ src/main/java/dao/EndangeredAnimalDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import models.EndangeredAnimal;
+
+import java.util.List;
+
 public interface EndangeredAnimalDao {
+
+    List<EndangeredAnimal> getAllEndangeredAnimals();
+    void addEndangeredAnimal(EndangeredAnimal endangeredAnimal);
+    EndangeredAnimal findEndangeredAnimalById(int id);
+    void updateEndangeredAnimal(EndangeredAnimal endangeredAnimal, String name, String health, String age);
+
+    void clearAllEndangeredAnimals();
 }
