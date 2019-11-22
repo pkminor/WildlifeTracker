99a45512b3ea82dbab98f8b3ca689a40247da16b
pkminor <minorpk@gmail.com>
2019-11-22 16:11:54 +0300

    added endangered animal model

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,4 +1,45 @@
 package models;
 
-public class EndangeredAnimal {
+import java.util.Objects;
+
+public class EndangeredAnimal extends  Animal {
+    private String health;
+    private String age;
+
+    public EndangeredAnimal(int id, String name, String type, String health, String age) {
+        super(id, name, type);
+        this.health = health;
+        this.age = age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+    public void setHealth(String health) {
+        this.health = health;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        EndangeredAnimal that = (EndangeredAnimal) o;
+        return Objects.equals(health, that.health) &&
+                Objects.equals(age, that.age) &&
+                Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getType(), that.getType()) &&
+                Objects.equals(getName(), that.getName()) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), health, age);
+    }
 }
