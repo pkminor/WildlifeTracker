a85e8fcd8dadee9dc5cae72caa32cd50c2a32f98
pkminor <minorpk@gmail.com>
2019-11-22 16:10:45 +0300

    updated animal model

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,48 @@
 package models;
 
+import java.util.Objects;
+
 public class Animal {
+    private int id;
+    private String name;
+    private String type;
+
+    public Animal(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id &&
+                Objects.equals(name, animal.name) &&
+                Objects.equals(type, animal.type);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type);
+    }
 }
