74aea68e532768300f2598c0ba155aa25c021065
pkminor <minorpk@gmail.com>
2019-11-23 10:12:31 +0300

    implemented tests to fetch animal/endangered, sighted/unsighted records. :) they all passed

--- src/test/java/dao/Sql2oSightingDaoTest.java
+++ src/test/java/dao/Sql2oSightingDaoTest.java
@@ -1,5 +1,7 @@
 package dao;
 
+import models.Animal;
+import models.EndangeredAnimal;
 import models.Sighting;
 import org.junit.*;
 import org.sql2o.Connection;
@@ -14,6 +16,8 @@ import static org.junit.Assert.*;
 public class Sql2oSightingDaoTest {
 
     private  static Sql2oSightingDao sightingDao;
+    private  static  Sql2oAnimalDao animalDao;
+    private static Sql2oEndangeredAnimalDao endangeredAnimalDao;
     private static Connection con;
 
     @BeforeClass
@@ -22,12 +26,21 @@ public class Sql2oSightingDaoTest {
         Sql2o sql2o = new Sql2o(connectionStr,"pkminor","password");
 
         sightingDao = new Sql2oSightingDao(sql2o);
+        animalDao = new Sql2oAnimalDao(sql2o);
+        endangeredAnimalDao =  new Sql2oEndangeredAnimalDao(sql2o);
+
         con = sql2o.open();
         sightingDao.clearAllSightings(); //start with empty table
+        animalDao.clearAllAnimals();
+        endangeredAnimalDao.clearAllEndangeredAnimals();
     }
 
     @After
-    public void tearDown() throws Exception { sightingDao.clearAllSightings();   }
+    public void tearDown() throws Exception {
+        sightingDao.clearAllSightings();
+        animalDao.clearAllAnimals();
+        endangeredAnimalDao.clearAllEndangeredAnimals();
+    }
 
     @AfterClass
     public static void shutDown() throws Exception { con.close(); }
@@ -42,6 +55,128 @@ public class Sql2oSightingDaoTest {
 
         assertEquals(2, sightingDao.getAllSightings().size());
         assertTrue(sightingDao.getAllSightings().containsAll(Arrays.asList(s1,s2)));
+    }
+
+    @Test
+    public void getSightedAnimals_ReturnsOnlySightedAnimals_True() {
+        Animal a1 = new Animal(1,"lion",animalDao.RECORD_TYPE);
+        Animal a2 = new Animal(2,"ostrich",animalDao.RECORD_TYPE);
+        animalDao.addAnimal(a1);
+        animalDao.addAnimal(a2);
+
+        EndangeredAnimal ea1 = new EndangeredAnimal(1,"elephant",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.OKAY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.ADULT.toString());
+
+        EndangeredAnimal ea2 = new EndangeredAnimal(2,"rhino",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.HEALTHY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.YOUNG.toString());
+
+        endangeredAnimalDao.addEndangeredAnimal(ea1);
+        endangeredAnimalDao.addEndangeredAnimal(ea2);
+
+        Sighting s1 = new Sighting(1,a1.getId(),"river","ryan",new Timestamp(new Date().getTime()));
+        Sighting s2 = new Sighting(2,ea1.getId(),"ridge","ryan",new Timestamp(new Date().getTime()));
+
+        sightingDao.addSighting(s1);
+        sightingDao.addSighting(s2);
+
+        assertTrue(sightingDao.getSightedAnimals().contains(a1)); //animal sighted
+        assertFalse(sightingDao.getSightedAnimals().contains(a2)); //animal not sighted
+        assertFalse(sightingDao.getSightedAnimals().contains(ea1)); //sighted but not an animal object
+        assertFalse(sightingDao.getSightedAnimals().contains(ea2)); //not sighted and not an animal object
+    }
+
+    @Test
+    public void getUnsightedAnimals_ReturnsOnlyUnsightedAnimals_True() {
+        Animal a1 = new Animal(1,"lion",animalDao.RECORD_TYPE);
+        Animal a2 = new Animal(2,"ostrich",animalDao.RECORD_TYPE);
+        animalDao.addAnimal(a1);
+        animalDao.addAnimal(a2);
+
+        EndangeredAnimal ea1 = new EndangeredAnimal(1,"elephant",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.OKAY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.ADULT.toString());
+
+        EndangeredAnimal ea2 = new EndangeredAnimal(2,"rhino",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.HEALTHY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.YOUNG.toString());
+
+        endangeredAnimalDao.addEndangeredAnimal(ea1);
+        endangeredAnimalDao.addEndangeredAnimal(ea2);
+
+        Sighting s1 = new Sighting(1,a1.getId(),"river","ryan",new Timestamp(new Date().getTime()));
+        Sighting s2 = new Sighting(2,ea1.getId(),"ridge","ryan",new Timestamp(new Date().getTime()));
+
+        sightingDao.addSighting(s1);
+        sightingDao.addSighting(s2);
+
+        assertFalse(sightingDao.getUnsightedAnimals().contains(a1)); //animal sighted
+        assertTrue(sightingDao.getUnsightedAnimals().contains(a2)); //animal not sighted
+        assertFalse(sightingDao.getUnsightedAnimals().contains(ea1)); //sighted not an animal object
+        assertFalse(sightingDao.getUnsightedAnimals().contains(ea2)); //not sighted but and not an animal object
+    }
+
+    @Test
+    public void getSightedEndangeredAnimals_ReturnsOnlySightedEndangeredAnimals_True() {
+        Animal a1 = new Animal(1,"lion",animalDao.RECORD_TYPE);
+        Animal a2 = new Animal(2,"ostrich",animalDao.RECORD_TYPE);
+        animalDao.addAnimal(a1);
+        animalDao.addAnimal(a2);
+
+        EndangeredAnimal ea1 = new EndangeredAnimal(1,"elephant",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.OKAY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.ADULT.toString());
+
+        EndangeredAnimal ea2 = new EndangeredAnimal(2,"rhino",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.HEALTHY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.YOUNG.toString());
+
+        endangeredAnimalDao.addEndangeredAnimal(ea1);
+        endangeredAnimalDao.addEndangeredAnimal(ea2);
+
+        Sighting s1 = new Sighting(1,a1.getId(),"river","ryan",new Timestamp(new Date().getTime()));
+        Sighting s2 = new Sighting(2,ea1.getId(),"ridge","ryan",new Timestamp(new Date().getTime()));
+
+        sightingDao.addSighting(s1);
+        sightingDao.addSighting(s2);
+
+        assertFalse(sightingDao.getSightedEndangeredAnimals().contains(a1)); //not endangered animal, sighted
+        assertFalse(sightingDao.getSightedEndangeredAnimals().contains(a2)); //not endangered animal, not sighted
+        assertTrue(sightingDao.getSightedEndangeredAnimals().contains(ea1)); //sighted, endangered animal object
+        assertFalse(sightingDao.getSightedEndangeredAnimals().contains(ea2)); //not sighted, endangered animal object
+    }
+
+    @Test
+    public void getUnsightedEndangeredAnimals_ReturnsOnlyUnsightedEndangeredAnimals_True() {
+
+        Animal a1 = new Animal(1,"lion",animalDao.RECORD_TYPE);
+        Animal a2 = new Animal(2,"ostrich",animalDao.RECORD_TYPE);
+        animalDao.addAnimal(a1);
+        animalDao.addAnimal(a2);
+
+        EndangeredAnimal ea1 = new EndangeredAnimal(1,"elephant",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.OKAY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.ADULT.toString());
+
+        EndangeredAnimal ea2 = new EndangeredAnimal(2,"rhino",
+                endangeredAnimalDao.RECORD_TYPE,
+                Sql2oEndangeredAnimalDao.HEALTHSTATUS.HEALTHY.toString(), Sql2oEndangeredAnimalDao.AGESTATUS.YOUNG.toString());
+
+        endangeredAnimalDao.addEndangeredAnimal(ea1);
+        endangeredAnimalDao.addEndangeredAnimal(ea2);
+
+        Sighting s1 = new Sighting(1,a1.getId(),"river","ryan",new Timestamp(new Date().getTime()));
+        Sighting s2 = new Sighting(2,ea1.getId(),"ridge","ryan",new Timestamp(new Date().getTime()));
+
+        sightingDao.addSighting(s1);
+        sightingDao.addSighting(s2);
+
+        assertFalse(sightingDao.getUnsightedEndangeredAnimals().contains(a1)); //not endangered animal, sighted
+        assertFalse(sightingDao.getUnsightedEndangeredAnimals().contains(a2)); //not endangered animal, not sighted
+        assertFalse(sightingDao.getUnsightedEndangeredAnimals().contains(ea1)); //sighted, endangered animal object
+        assertTrue(sightingDao.getUnsightedEndangeredAnimals().contains(ea2)); //not sighted, endangered animal object
+
     }
 
     @Test
@@ -77,7 +212,7 @@ public class Sql2oSightingDaoTest {
 
     }
 
-    @Test
+    @Test //(expected = IllegalArgumentException.class) //-> this is causing issues????
     public void updateSighting_SetsAidLocationRangernameUpdates() {
         Sighting s1 = setupSighting();
         Sighting s2 = setupSighting();
