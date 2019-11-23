8e3b4323283c3270f8e5bd51e28ab16ad37b0088
pkminor <minorpk@gmail.com>
2019-11-23 12:50:22 +0300

    added home route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class App {
+    public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 7654;
+        }
+
+        port(port);
+
+        staticFileLocation("/public");
+
+        get("/", (req,res)->index(), new HandlebarsTemplateEngine());
+
+    }
+
+    private static ModelAndView index(){
+        Map<String, Object> model = new HashMap<String,Object>();
+
+        return new ModelAndView(model,"index.hbs");
+    }
 }
