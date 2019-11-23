aebfdc0413af170c093f54476f9ffb9ca3393c31
pkminor <minorpk@gmail.com>
2019-11-23 07:09:36 +0300

    added Sighting model, Sighting Dao, Sighting Dao Test

---
+++ src/test/java/dao/Sql2oSightingDaoTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oSightingDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getAllSightings() {
+    }
+
+    @Test
+    public void addSighting() {
+    }
+
+    @Test
+    public void findSightingById() {
+    }
+
+    @Test
+    public void updateSighting() {
+    }
+
+    @Test
+    public void clearAllSightings() {
+    }
+}
